fb75eba3f31d8f16549caf3efbf3aeee9ab06250
willis <songkai3908032@163.com>
2020-07-27 00:26:50 +0800

    update

--- nio/src/main/java/com/Client.java
+++ nio/src/main/java/com/Client.java
@@ -2,13 +2,17 @@ package com;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.charset.Charset;
 
 public class Client {
     public static void main(String[] args) {
         try {
             Socket client = new Socket("127.0.0.1", 9000);
             InputStream inputStream = client.getInputStream();
+            OutputStream outputStream = client.getOutputStream();
+            outputStream.write("hello, i am willis".getBytes(Charset.forName("utf-8")));
             byte[] bytes = new byte[1024];
             while (true) {
                 int r = inputStream.read(bytes, 0, bytes.length);
--- nio/src/main/java/com/nio/netty/NettyNioServer.java
+++ nio/src/main/java/com/nio/netty/NettyNioServer.java
@@ -7,7 +7,6 @@ import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
 
 import java.net.InetSocketAddress;
 import java.nio.charset.Charset;
@@ -21,9 +20,14 @@ public class NettyNioServer {
             server.group(group)
                     .channel(NioServerSocketChannel.class)
                     .localAddress(new InetSocketAddress(9000))
+                    /**
+                     * 当channel被创建时，他会被自动分配到它专属的pipeline
+                     * initChannel被调用时，ChannelInitializer将在ChannelPipeline中安装一组自定义的ChannelHandler
+                     */
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.writeAndFlush(Unpooled.copiedBuffer("test".getBytes(Charset.forName("utf-8"))));
                             /**
                              * channelpipline 为channelHandler链提供了容器，并定义了用于在该链上传播入站和出站的事件流的api
                              */
@@ -33,13 +37,23 @@ public class NettyNioServer {
                                     ctx.writeAndFlush(byteBuf.duplicate());
 
                                     ByteBuf byteBuf2 = Unpooled.copiedBuffer("hello, this is nio netty implement2".getBytes(Charset.forName("utf-8")));
-                                    ctx.writeAndFlush(byteBuf2.duplicate())
-                                            //监听写动作，写完就执行close操作（关闭连接）
-                                            .addListener(ChannelFutureListener.CLOSE);
+                                    ctx.writeAndFlush(byteBuf2.duplicate());
+                                            //监听写动作，写完就执行close操作（关闭连接）;
 
                                     ByteBuf byteBuf3 = Unpooled.copiedBuffer("hello, this is nio netty implement3".getBytes(Charset.forName("utf-8")));
                                     ctx.writeAndFlush(byteBuf3.duplicate());
                                 }
+                            }).addLast(new ChannelInboundHandlerAdapter() {
+                                @Override
+                                public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                    ByteBuf msgb = (ByteBuf) msg;
+                                    String msgContent = msgb.toString(Charset.forName("utf-8"));
+                                    System.out.println("receive msg:" + msgContent);
+
+                                    ctx.writeAndFlush(Unpooled.copiedBuffer("我已经收到了你的消息，再见~".getBytes(Charset.forName("utf-8"))))
+                                            .addListener(ChannelFutureListener.CLOSE);
+                                }
+
                             });
                         }
                     });
